33b7d86fe7a1dfd0e2cb5e52f07b5b4c651cf9a4
Jorge <aries.george@hotmail.com>
2015-10-30 08:55:36 -0600

    Added operation change after correct answer, happy and sad faces depending on answer and introduction audio

--- src/Preloader.js
+++ src/Preloader.js
@@ -11,9 +11,25 @@ TuxGame.Preloader.prototype = {
 		this.load.setPreloadSprite(this.preloadBar);
 		// load images
 		this.load.image('beach-bg', 'img/block1/game1/beach-bg.png');
+		this.load.image('home_button', 'img/block1/game1/home_button.png');
 		this.load.image('avatar', 'img/block1/game1/avatar.png');
 		this.load.image('cannon', 'img/block1/game1/cannon.png');
+		this.load.image('cannon_ball', 'img/block1/game1/cannon_ball.png');
 		this.load.image('pirate_boat', 'img/block1/game1/pirate_boat.png');
+		this.load.image('pirate_boat2', 'img/block1/game1/pirate_boat2.png');
+		this.load.image('pirate_boat3', 'img/block1/game1/pirate_boat3.png');
+		this.load.image('destroyed_boat', 'img/block1/game1/destroyed_boat.png');
+		this.load.image('destroyed_boat2', 'img/block1/game1/destroyed_boat2.png');
+		this.load.image('destroyed_boat3', 'img/block1/game1/destroyed_boat3.png');
+		this.load.image('bubble1', 'img/block1/game1/bubble1.png');
+		this.load.image('bubble2', 'img/block1/game1/bubble2.png');
+		this.load.image('bubble3', 'img/block1/game1/bubble3.png');
+		this.load.image('coins', 'img/block1/game1/coins.png');
+		this.load.image('expressions_bubble', 'img/block1/game1/expressions_bubble.png');
+		this.load.image('happy_penguin_face', 'img/block1/game1/happy_penguin_face.png');
+		this.load.image('sad_penguin_face', 'img/block1/game1/sad_penguin_face.png');
+		// load sounds
+		this.load.audio('intro', 'sound/block1/game1/intro.m4a');
 	},
 	create: function(){
 		// start the MainMenu state
--- src/block1/Game1.js
+++ src/block1/Game1.js
@@ -1,13 +1,14 @@
 var message;
 var instruction;
 var scoreText;
-var score = 0;
+var reaction = 0;
+var score = 30;
 var number1, number2, number3;
 var image, image2, image3;
-var operations = ["2+3", "1+1", "4*2"];
-var results = ["5", "2", "8"];
+var operations = ["2+3", "1+1", "4*2", "6*5", "8-4", "9-3", "3*4", "9+9", "8-5"];
+var results = ["5", "2", "8", "30", "4", "6", "12", "18", "3"];
 // Between 0 and 2
-var randomNumber1 = Math.floor((Math.random() * 3));
+var randomNumber1 = Math.floor((Math.random() * 9));
 // Another number between 0 and 2
 var randomNumber2 = Math.floor((Math.random() * 3));
 // Between 1 and 2
@@ -31,53 +32,61 @@ TuxGame.Block1Game1 = function(game){
 TuxGame.Block1Game1.prototype = {
 	create: function(){
 		this.add.sprite(0, 0, 'beach-bg');
-		sprite = this.add.sprite(5, 500, 'avatar');
-		sprite.scale.setTo(0.15, 0.15);
-		sprite = this.add.sprite(100, 530, 'cannon');
-		sprite.scale.setTo(0.15, 0.15);
-		image = this.add.sprite(450, 320, 'pirate_boat');
-		image.scale.setTo(0.75, 0.75);
-		image2 = this.add.sprite(180, 420, 'pirate_boat');
-		image2.scale.setTo(0.75, 0.75);
-		image3 = this.add.sprite(600, 500, 'pirate_boat');
-		image3.scale.setTo(0.75, 0.75);
+		home_button = this.add.sprite(0, 0, 'home_button');
+		home_button.scale.setTo(0.07, 0.07);
+		sprite = this.add.sprite(180, 280, 'avatar');
+		sprite.scale.setTo(0.20, 0.20);
+		sprite = this.add.sprite(280, 410, 'cannon_ball');
+		sprite.scale.setTo(0.25, 0.25);
+		sprite = this.add.sprite(180, 370, 'cannon');
+		sprite.scale.setTo(0.25, 0.25);
+		sprite = this.add.sprite(20, 50, 'expressions_bubble');
+		sprite.scale.setTo(0.30, 0.30);
+		bubble3 = this.add.sprite(680, 16, 'coins');
+		bubble3.scale.setTo(0.08, 0.08);
+		boat1 = this.add.sprite(450, 230, 'pirate_boat');
+		boat1.scale.setTo(0.60, 0.60);
+		boat2 = this.add.sprite(600, 270, 'pirate_boat2');
+		boat2.scale.setTo(0.55, 0.55);
+		boat3 = this.add.sprite(610, 370, 'pirate_boat3');
+		boat3.scale.setTo(0.80, 0.80);
+		bubble1 = this.add.sprite(480, 150, 'bubble1');
+		bubble1.scale.setTo(0.15, 0.15);
+		bubble2 = this.add.sprite(650, 180, 'bubble2');
+		bubble2.scale.setTo(0.15, 0.15);
+		bubble3 = this.add.sprite(690, 320, 'bubble3');
+		bubble3.scale.setTo(0.15, 0.15);
 
-		image.inputEnabled = true;
-	    image2.inputEnabled = true;
-	    image3.inputEnabled = true;
+		home_button.inputEnabled = true;
+		boat1.inputEnabled = true;
+	    boat2.inputEnabled = true;
+	    boat3.inputEnabled = true;
+		bubble1.inputEnabled = true;
+	    bubble2.inputEnabled = true;
+	    bubble3.inputEnabled = true;
 
-		message = this.add.text(250, 16, '', { fill: '#ffffff' });
-	    scoreText = this.add.text(800, 16, '', { fill: '#ffffff' });
-	    instruction = this.add.text(300, 100, '', { fill: '#ffffff' });
-	    instruction.text = "Select the " + operations[randomNumber1] + " boat";
-		// setPossibleAnswers();
-	},
-	managePause: function(){
-		// pause the game
-		this.game.paused = true;
-		// add proper informational text
-		var pausedText = this.add.text(100, 250, "Game paused.\nTap anywhere to continue.", this._fontStyle);
-		// set event listener for the user's click/tap the screen
-		this.input.onDown.add(function(){
-			// remove the pause text
-			pausedText.destroy();
-			// unpause the game
-			this.game.paused = false;
-		}, this);
-	},
-	update: function(){
+		//music = this.add.audio('intro');
 
+		//music.play();
+
+		message = this.add.text(50, 120, '', { font:'bold 40pt French Script MT', fill: '#000000' });
+	    scoreText = this.add.text(715, 0, '30', { font:'bold 40pt French Script MT', fill: '#000000' });
+	    instruction = this.add.text(200, 480, '', { font:'bold 50pt French Script MT', fill: '#000000' });
+	    instruction.text = operations[randomNumber1];
+		this.setPossibleAnswers();
 	},
 	setPossibleAnswers: function() {
-	    number1 = this.add.text(100, 100, '', { fill: '#ffffff' });
+	    number1 = this.add.text(515, 150, '', {  font:'bold 40pt French Script MT', fill: '#000000' });
 	    number1.text = possibleAnswers[randomNumber2];
-	    number2 = this.add.text(250, 250, '', { fill: '#ffffff' });
+	    number2 = this.add.text(695, 180, '', {  font:'bold 40pt French Script MT', fill: '#000000'  });
+	    //Check if the first number is the true answer
 	    if (number1.text == possibleAnswers[0]) {
 	        number2.text = possibleAnswers[randomNumber3];
 	    } else {
+	    	//The second number becomes the true answer
 	        number2.text = possibleAnswers[0];
 	    }
-	    number3 = this.add.text(400, 400, '', { fill: '#ffffff' });
+	    number3 = this.add.text(720, 320, '', {  font:'bold 40pt French Script MT', fill: '#000000'  });
 	    if (number1.text == possibleAnswers[0] || number2.text == possibleAnswers[0]) {
 	        if (number1.text == possibleAnswers[1] || number2.text == possibleAnswers[1]) {
 	            number3.text = possibleAnswers[2];
@@ -87,30 +96,64 @@ TuxGame.Block1Game1.prototype = {
 	    } else {
 	        number3.text = possibleAnswers[0];
 	    }
+	    //Add the events to the boats according to which number has the true answer
 	    if (number1.text == possibleAnswers[0]) {
-	        image.events.onInputDown.add(increaseScore, this);
-	        image2.events.onInputDown.add(decreaseScore, this);
-	        image3.events.onInputDown.add(decreaseScore, this);
+	        boat1.events.onInputDown.add(this.increaseScore, this);
+	        boat2.events.onInputDown.add(this.decreaseScore, this);
+	        boat3.events.onInputDown.add(this.decreaseScore, this);
 	    }
 	    if (number2.text == possibleAnswers[0]) {
-	        image.events.onInputDown.add(decreaseScore, this);
-	        image2.events.onInputDown.add(increaseScore, this);
-	        image3.events.onInputDown.add(decreaseScore, this);
+	        boat1.events.onInputDown.add(this.decreaseScore, this);
+	        boat2.events.onInputDown.add(this.increaseScore, this);
+	        boat3.events.onInputDown.add(this.decreaseScore, this);
 	    }
 	    if (number3.text == possibleAnswers[0]) {
-	        image.events.onInputDown.add(decreaseScore, this);
-	        image2.events.onInputDown.add(decreaseScore, this);
-	        image3.events.onInputDown.add(increaseScore, this);
+	        boat1.events.onInputDown.add(this.decreaseScore, this);
+	        boat2.events.onInputDown.add(this.decreaseScore, this);
+	        boat3.events.onInputDown.add(this.increaseScore, this);
 	    }
 	},
 	increaseScore: function() {
-	    score++;
-	    message.text = "Nice done.";
-	    scoreText.text = "Score: " + score;
+		if(reaction == 0)
+			reaction = this.add.sprite(100, 90, 'happy_penguin_face');
+		else{
+			reaction.x = 100;
+			reaction.loadTexture('happy_penguin_face', 0, false);
+		}
+		reaction.scale.setTo(1.70, 1.70);
+	    score+=10;
+	    scoreText.text = score;
+	    this.changeQuestion();
 	},
 	decreaseScore: function() {
-	    score--;
-	    message.text = "Oops, wrong answer.";
-	    scoreText.text = "Score: " + score;
+		if(reaction == 0)
+			reaction = this.add.sprite(70, 90, 'sad_penguin_face');
+		else{
+			reaction.loadTexture('sad_penguin_face', 0, false);
+			reaction.x = 70;
+		}
+		reaction.scale.setTo(1.70, 1.70);
+	    score-=10;
+	    scoreText.text = score;
+	},
+	changeQuestion: function(){
+		// Between 0 and 2
+		randomNumber1 = Math.floor((Math.random() * 9));
+		// Another number between 0 and 2
+		randomNumber2 = Math.floor((Math.random() * 3));
+		// Between 1 and 2
+		randomNumber3 = Math.floor((Math.random() * 2) + 1);
+		rightAnswer = results[randomNumber1];
+		wrongAnswer1 = Math.floor((Math.random() * 100) + 1);
+		wrongAnswer2 = Math.floor((Math.random() * 100) + 1);
+		possibleAnswers = [rightAnswer, wrongAnswer1, wrongAnswer2];
+		instruction.text = operations[randomNumber1];
+		number1.text = "";
+		number2.text = "";
+		number3.text = "";
+		boat1.events.onInputDown.removeAll();
+		boat2.events.onInputDown.removeAll();
+		boat3.events.onInputDown.removeAll();
+		this.setPossibleAnswers();
 	}
 };
