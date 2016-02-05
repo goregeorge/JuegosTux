12257b6864497cf09653c3fff1dd1213f63c9d28
Miguel Coronel <m.coronel.seg@gmail.com>
2016-02-05 01:28:31 -0600

    Add home button to game 1 block 2

--- src/block2/game1/displayers/CommonInfoDisplayer.js
+++ src/block2/game1/displayers/CommonInfoDisplayer.js
@@ -1,9 +1,10 @@
 CommonInfoDisplayer = function(){
-
+    var globalContext;
 };
 
 CommonInfoDisplayer.prototype = {
   displayInfo : function (context, name) {
+    globalContext = context;
     context.add.sprite(0, 0, 'point');
     context.add.text(50, 10, "Nivel " + name, context._fontStyle3);
     context.add.sprite(680, 175, 'score-half');
@@ -13,8 +14,11 @@ CommonInfoDisplayer.prototype = {
     wrongs.scale.setTo(0.7, 0.7);
     context.add.text(760, 200, TuxGame._correct, context._fontStyle2);
     wrongsText = context.add.text(760, 260, TuxGame._incorrect, context._fontStyle2);
-
+    context.add.button(700, 500, 'button-home', this.backToHome, this, 1, 0, 2);
     return wrongsText;
+  },
+  backToHome: function () {
+    globalContext.state.start('MainMenu');
   }
 
 };
