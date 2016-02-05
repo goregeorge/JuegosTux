3f721cba06a40920723f15b7420f8d6d0aab4838
Miguel Coronel <m.coronel.seg@gmail.com>
2016-02-05 00:37:07 -0600

    Buttons poinys to correct games

--- src/block3/game3/Block3Game3_Q1.js
+++ src/block3/game3/Block3Game3_Q1.js
@@ -40,7 +40,6 @@ TuxGame.Block3Game3_Q1.prototype = {
 	},
 
 	create : function(){
-		this.add.audio('loop').loopFull(1);
 		input.setAttribute("type", "number");
 
 		if (level2_3fail == false) {
--- src/menu/MainMenu.js
+++ src/menu/MainMenu.js
@@ -17,38 +17,51 @@ TuxGame.MainMenu.prototype = {
 
     // Bloque 1
     this.add.text(320, 110, "Bloque 1", this._fontStyle3);
-    game1block1 = this.add.button(140, 160, 'level-main-menu2', this.goGame1, this, 1, 0, 2);
+    game1block1 = this.add.button(140, 160, 'level-main-menu2', this.goGame1Block1, this, 1, 0, 2);
     game1block1.scale.setTo(0.4, 0.4);
     this.add.text(190, 180, "Juego 1", this._fontStyle2);
 
-    game2block1 = this.add.button(420, 160, 'level-main-menu2', this.goGame1, this, 1, 0, 2);
+    game2block1 = this.add.button(420, 160, 'level-main-menu2', this.goGame2Block1, this, 1, 0, 2);
     game2block1.scale.setTo(0.4, 0.4);
     this.add.text(480, 180, "Juego 2", this._fontStyle2);
 
     // Bloque 2
     this.add.text(320, 280, "Bloque 2", this._fontStyle3);
-    game1block2 = this.add.button(140, 330, 'level-main-menu2', this.goGame1, this, 1, 0, 2);
+    game1block2 = this.add.button(140, 330, 'level-main-menu2', this.goGame1Block2, this, 1, 0, 2);
     game1block2.scale.setTo(0.4, 0.4);
     this.add.text(190, 350, "Juego 1", this._fontStyle2);
 
-    game2block2 = this.add.button(420, 330, 'level-main-menu2', this.goGame1, this, 1, 0, 2);
+    game2block2 = this.add.button(420, 330, 'level-main-menu2', this.goGame2Block2, this, 1, 0, 2);
     game2block2.scale.setTo(0.4, 0.4);
     this.add.text(480, 350, "Juego 2", this._fontStyle2);
 
     // Bloque 3
     this.add.text(320, 450, "Bloque 3", this._fontStyle3);
-    game1block3 = this.add.button(140, 500, 'level-main-menu2', this.goGame1, this, 1, 0, 2);
+    game1block3 = this.add.button(140, 500, 'level-main-menu2', this.goGame1Block3, this, 1, 0, 2);
     game1block3.scale.setTo(0.4, 0.4);
     this.add.text(190, 520, "Juego 1", this._fontStyle2);
 
-    game2block3 = this.add.button(420, 500, 'level-main-menu2', this.goGame1, this, 1, 0, 2);
+    game2block3 = this.add.button(420, 500, 'level-main-menu2', this.goGame2Block3, this, 1, 0, 2);
     game2block3.scale.setTo(0.4, 0.4);
     this.add.text(480, 520, "Juego 2", this._fontStyle2);
     
   },
-  goGame1: function(){
+  goGame1Block1: function(){
+    this.state.start('Block1Game1');
+  },
+  goGame2Block1: function(){
+    this.state.start('Block1Game2');
+  },
+  goGame1Block2: function(){
+    this.state.start('Block2Welcome');
+  },
+  goGame2Block2: function(){
+    this.state.start('Block2Game2Welcome');
+  },
+  goGame1Block3: function(){
     this.state.start('MenuB3G2');
   },
-  update: function(){
-  }
+  goGame2Block3: function(){
+    this.state.start('MenuB3G3');
+  },
 };
