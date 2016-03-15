1ca213b730a3b01532658526b24f1be0713bc113
Miguel Coronel <m.coronel.seg@gmail.com>
2016-03-15 14:04:56 -0600

    Add more text dude

--- src/menu/MainMenu.js
+++ src/menu/MainMenu.js
@@ -25,9 +25,10 @@ TuxGame.MainMenu.prototype = {
 
     this.physics.startSystem(Phaser.Physics.ARCADE);
     this.physics.arcade.gravity.y = 200;
-    this._fontStyle2 = { font: "35px Arial", fill: "#FFCC00", stroke: "#333", strokeThickness: 5, align: "center" };
     this._fontStyle = { font: "50px Arial", fill: "#FFFFFF", stroke: "#333", strokeThickness: 5, align: "center" };
+    this._fontStyle2 = { font: "35px Arial", fill: "#FFCC00", stroke: "#333", strokeThickness: 5, align: "center" };
     this._fontStyle3 = { font: "40px Arial", fill: "#FFFFFF", stroke: "#333", strokeThickness: 5, align: "center" };
+    this._fontStyle4 = { font: "25px Arial", fill: "#FFCC00", stroke: "#333", strokeThickness: 5, align: "center" };
 
     this.add.sprite(0, 0, 'bg-main-menu');
 
@@ -37,17 +38,19 @@ TuxGame.MainMenu.prototype = {
     tux.scale.setTo(0.15, 0.15);
 
     // Bloque 1
-    this.add.text(320, 110, "Bloque 1", this._fontStyle3);
-    game1block1 = this.add.button(140, 160, 'level-main-menu2', this.goGame1Block1, this, 1, 0, 2);
+    this.add.text(320, 90, "Bloque 1", this._fontStyle3);
+    this.add.text(250, 130, "Sumas, restas y fracciones", this._fontStyle4);
+    game1block1 = this.add.button(140, 165, 'level-main-menu2', this.goGame1Block1, this, 1, 0, 2);
     game1block1.scale.setTo(0.4, 0.4);
     this.add.text(190, 180, "Juego 1", this._fontStyle2);
 
-    game2block1 = this.add.button(420, 160, 'level-main-menu2', this.goGame2Block1, this, 1, 0, 2);
+    game2block1 = this.add.button(420, 165, 'level-main-menu2', this.goGame2Block1, this, 1, 0, 2);
     game2block1.scale.setTo(0.4, 0.4);
     this.add.text(480, 180, "Juego 2", this._fontStyle2);
 
     // Bloque 2
-    this.add.text(320, 280, "Bloque 2", this._fontStyle3);
+    this.add.text(320, 255, "Bloque 2", this._fontStyle3);
+    this.add.text(150, 295, "Recta númerica y simplificación de fracciones", this._fontStyle4);
     game1block2 = this.add.button(140, 330, 'level-main-menu2', this.goGame1Block2, this, 1, 0, 2);
     game1block2.scale.setTo(0.4, 0.4);
     this.add.text(190, 350, "Juego 1", this._fontStyle2);
@@ -57,12 +60,13 @@ TuxGame.MainMenu.prototype = {
     this.add.text(480, 350, "Juego 2", this._fontStyle2);
 
     // Bloque 3
-    this.add.text(320, 450, "Bloque 3", this._fontStyle3);
-    game1block3 = this.add.button(140, 500, 'level-main-menu2', this.goGame1Block3, this, 1, 0, 2);
+    this.add.text(320, 430, "Bloque 3", this._fontStyle3);
+    this.add.text(110, 470, "Suma, resta de fracciones y conversión de unidades", this._fontStyle4);
+    game1block3 = this.add.button(140, 510, 'level-main-menu2', this.goGame1Block3, this, 1, 0, 2);
     game1block3.scale.setTo(0.4, 0.4);
     this.add.text(190, 520, "Juego 1", this._fontStyle2);
 
-    game2block3 = this.add.button(420, 500, 'level-main-menu2', this.goGame2Block3, this, 1, 0, 2);
+    game2block3 = this.add.button(420, 510, 'level-main-menu2', this.goGame2Block3, this, 1, 0, 2);
     game2block3.scale.setTo(0.4, 0.4);
     this.add.text(480, 520, "Juego 2", this._fontStyle2);
     
